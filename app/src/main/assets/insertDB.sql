64c15bb33526c7396d208b1224bbfe076e3d400a
Florian Leriche <florian.leriche@insa-rouen.fr>
2016-09-29 22:25:12 +0200

    modif db

---
+++ app/src/main/assets/insertDB.sql
@@ -0,0 +1,27 @@
+--Attaque
+INSERT INTO heroes (name, health, armor, shield, real_name, height, occupation, base_of_operation, affiliation, summary, quote, difficulty, id_role) VALUES ('Faucheur', 250, 0, 0, 'Gabriel Reyes', 1);
+
+INSERT INTO heroes (name, difficulty) VALUES ('Genji', 3);
+INSERT INTO heroes (name, difficulty) VALUES ('McCree', 2);
+INSERT INTO heroes (name, difficulty) VALUES ('Pharah', 1);
+INSERT INTO heroes (name, difficulty) VALUES ('Soldat76', 1);
+INSERT INTO heroes (name, difficulty) VALUES ('Tracer', 2);
+--Defense
+INSERT INTO heroes (name, difficulty) VALUES ('Bastion', 1);
+INSERT INTO heroes (name, difficulty) VALUES ('Hanzo', 3);
+INSERT INTO heroes (name, difficulty) VALUES ('Junkrat', 2);
+INSERT INTO heroes (name, difficulty) VALUES ('Mei', 3);
+INSERT INTO heroes (name, difficulty) VALUES ('Tobjörn', 2);
+INSERT INTO heroes (name, difficulty) VALUES ('Fatale', 2);
+--Tank
+INSERT INTO heroes (name, difficulty) VALUES ('D.Va', 2);
+INSERT INTO heroes (name, difficulty) VALUES ('Reinhardt', 1);
+INSERT INTO heroes (name, difficulty) VALUES ('Roadhog', 1);
+INSERT INTO heroes (name, difficulty) VALUES ('Winston', 2);
+INSERT INTO heroes (name, difficulty) VALUES ('Zarya', 3);
+--Support
+INSERT INTO heroes (name, difficulty) VALUES ('Ana', 3);
+INSERT INTO heroes (name, difficulty) VALUES ('Lúcio', 2);
+INSERT INTO heroes (name, difficulty) VALUES ('Mercy', 1);
+INSERT INTO heroes (name, difficulty) VALUES ('Symmetra', 2);
+INSERT INTO heroes (name, difficulty) VALUES ('Zenyatta', 3);
---
+++ app/src/main/java/com/onlywatch/fleriche/onlywatch/BlankFragment.java
@@ -0,0 +1,43 @@
+package com.onlywatch.fleriche.onlywatch;
+
+
+import android.app.Fragment;
+import android.content.Context;
+import android.os.Bundle;
+import android.support.v7.widget.GridLayoutManager;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.onlywatch.fleriche.onlywatch.Entity.Heroes;
+import com.onlywatch.fleriche.onlywatch.database.DatabaseHandler;
+import com.onlywatch.fleriche.onlywatch.database.HeroesManager;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class BlankFragment extends Fragment {
+
+
+    public BlankFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_blank, container, false);
+        TextView tv = (TextView) view.findViewById(R.id.singe);
+
+        //getActivity().deleteDatabase("onlywatch.db");
+        HeroesManager hm = new HeroesManager(getActivity());
+        hm.open();
+        Heroes heroes = hm.getHeroe(19);
+        tv.setText(heroes.getNom());
+        hm.close();
+        return view;
+    }
+}
+
