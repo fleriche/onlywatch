970e20c72651cfac4a5c325ddc37795b8437bf77
Florian Leriche <florian.leriche@insa-rouen.fr>
2016-11-02 23:59:53 +0100

    generalites + header drawer

--- app/src/main/java/com/onlywatch/fleriche/onlywatch/HeroesGeneralityFragment.java
+++ app/src/main/java/com/onlywatch/fleriche/onlywatch/HeroesGeneralityFragment.java
@@ -3,6 +3,9 @@ package com.onlywatch.fleriche.onlywatch;
 
 import android.app.Fragment;
 import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.drawable.ClipDrawable;
 import android.os.Bundle;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.widget.GridLayoutManager;
@@ -13,6 +16,7 @@ import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.ScrollView;
 import android.widget.TextView;
+import android.widget.ViewFlipper;
 
 import com.onlywatch.fleriche.onlywatch.Entity.Heroes;
 import com.onlywatch.fleriche.onlywatch.database.DatabaseHandler;
@@ -23,6 +27,10 @@ import com.onlywatch.fleriche.onlywatch.database.HeroesManager;
  * A simple {@link Fragment} subclass.
  */
 public class HeroesGeneralityFragment extends android.support.v4.app.Fragment {
+    private static final int EASY = 1;
+    private static final int MEDIUM = 2;
+    private static final int HARD = 3;
+
     public HeroesGeneralityFragment() {}
 
     public static HeroesGeneralityFragment newInstance(int heroesid) {
@@ -39,7 +47,8 @@ public class HeroesGeneralityFragment extends android.support.v4.app.Fragment {
         View view = inflater.inflate(R.layout.fragment_blank, container, false);
 
         TextView tvRole = (TextView) view.findViewById(R.id.role);
-        TextView tvDifficulty = (TextView) view.findViewById(R.id.difficulty);
+        ViewFlipper viewFlipperDifficulty2 = (ViewFlipper) view.findViewById(R.id.viewFlipperDifficulty2);
+        ViewFlipper viewFlipperDifficulty3 = (ViewFlipper) view.findViewById(R.id.viewFlipperDifficulty3);
         int heroesId = getArguments().getInt("heroesId");
         HeroesManager hm = new HeroesManager(getActivity());
         Heroes heroes;
@@ -47,7 +56,18 @@ public class HeroesGeneralityFragment extends android.support.v4.app.Fragment {
         hm.open();
         heroes = hm.getHeroe(heroesId);
         tvRole.setText(heroes.getRole().toUpperCase());
-        tvDifficulty.setText(""+heroes.getDifficulty());
+
+        switch (heroes.getDifficulty()) {
+            case EASY:
+                viewFlipperDifficulty2.showNext();
+                viewFlipperDifficulty3.showNext();
+                break;
+            case MEDIUM:
+                viewFlipperDifficulty3.showNext();
+                break;
+            default:
+                break;
+        }
         hm.close();
         return view;
     }
